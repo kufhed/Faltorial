6bcf1edbbd70639a904e0f3f7b4be90e0c272bda
Kukuh Frehadtomo <kufhed@gmail.com>
2014-05-13 11:54:12 +0700

    add tampilan

--- Faktorial.java
+++ Faktorial.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
 class hitung
 {
     public static int hsl(int nilai)
     {
+        System.out.println("Faktorial dari "+nilai+"! : ");
+        
         int i,tot=1;
         for (i=1;i<=nilai;i++)
         {
             tot=tot*i;
         }
-        System.out.println("Faktorial dari "+nilai+"! : ");
+        
         return(tot);
     }
 };
@@ -16,9 +19,15 @@ class faktorial
 {
     public static void main(String[] args) 
     {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
         int hasil;
         hitung ht=new hitung();
-        hasil=ht.hsl(7);
+        hasil=ht.hsl(n);
+        for(int i=n; i>0; i++)
+        {
+            System.out.print(i);
+        }
         System.out.println("Hasil Faktorial = "+hasil);
     }
 }
